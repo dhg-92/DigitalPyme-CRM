db42c707d316aaf749bc55589858e101b5aab9f9
Diego Higuera <dh92@uoc.edu>
2025-05-04 11:19:49 +0100

    Fix products in offerdetails

---
+++ Offers/src/main/java/opensuite/crm/offers/application/rest/request/UpdateStatusOfferRequest.java
@@ -0,0 +1,18 @@
+package opensuite.crm.offers.application.rest.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+public class UpdateStatusOfferRequest {
+
+        @NotNull
+        private String status;
+
+}
