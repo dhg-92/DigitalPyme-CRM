4e70deba4191896e609c5d5da748ebf6e812eded
dhg-92 <dh92@uoc.edu>
2025-05-12 19:31:59 +0100

    Update schema.sql

--- Users/src/main/resources/schema.sql
+++ Users/src/main/resources/schema.sql
@@ -1,3 +1,2 @@
 INSERT INTO "user" (name, surname, email, password, phone, is_admin)
-VALUES ('Admin', 'Admin', 'diegohg8@gmail.com', '123456', 666666666, '1')
-        , ('User', 'User', 'user@user.com', '123456', 666666666, '0');
+VALUES ('Admin', 'Admin', 'admin@opensuitecrm.com', '123456', 666666666, '1');
