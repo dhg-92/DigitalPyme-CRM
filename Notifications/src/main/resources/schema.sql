32c711b061f24bfb3495995a3f0eb0c94ec39c07
dhg-92 <dh92@uoc.edu>
2025-05-18 22:48:12 +0100

    Update schema.sql

--- Notifications/src/main/resources/schema.sql
+++ Notifications/src/main/resources/schema.sql
@@ -1,2 +1,2 @@
 INSERT INTO smtp_config (auth, port, use_ssl, protocol, host, password, username)
-VALUES (false, 465, true, 'smtp', 'smtp.serviciodecorreo.es', '', 'noreply@digipymcrm.es');
+VALUES (false, 465, true, 'smtp', 'smtp.dondominio.com', '', 'noreply@digipymcrm.es');
