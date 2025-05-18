c8eac250913dc83969979b4a69055593826a2693
dhg-92 <dh92@uoc.edu>
2025-05-18 22:41:28 +0100

    Update schema.sql

--- Notifications/src/main/resources/schema.sql
+++ Notifications/src/main/resources/schema.sql
@@ -1,2 +1,2 @@
 INSERT INTO smtp_config (auth, port, use_ssl, protocol, host, password, username)
-VALUES (false, 0, false, '', '', '', '');
+VALUES (false, 465, true, 'smtp', 'smtp.serviciodecorreo.es', '', 'noreply@digipymcrm.es');
